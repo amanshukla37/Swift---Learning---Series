68504bbe6eb1e38b1021856bdf61ded1e5396b85
amanshukla37 <119480441+amanshukla37@users.noreply.github.com>
2025-02-19 18:31:34 +0530

    01_variables_and_constants.swift

---
+++ Day1/01_variables_and_constants.swift
@@ -0,0 +1,19 @@
+// Variables and Constants in Swift
+
+import Foundation
+
+// Constant (Immutable)
+let pi: Double = 3.14159
+let appName = "Swift Learning Series"
+
+// Variable (Mutable)
+var username = "Aman"
+
+// Data Types
+var isStudent: Bool = true
+var temperature: Float = 36.5
+
+// Printing values
+print("App Name: \(appName)")
+print("User Name: \(username)")
+print("Temperature: \(temperature)°C, Is Student: \(isStudent)")
